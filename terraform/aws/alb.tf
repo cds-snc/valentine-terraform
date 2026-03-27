2b5a6a40eebd93bd074928bacbc7335824e4b72f
Pat Heard <patrick.heard@cds-snc.ca>
2026-03-27 16:11:42 -0400

    fix: adjust healthcheck values as they were invalid (#32)
    The interval must be > than the timeout.

--- terraform/aws/alb.tf
+++ terraform/aws/alb.tf
@@ -10,9 +10,9 @@ resource "aws_lb_target_group" "valentine" {
     enabled             = true
     interval            = 10
     path                = "/"
-    timeout             = 30
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
+    timeout             = 5
+    healthy_threshold   = 4
+    unhealthy_threshold = 4
   }
 
   tags = {
